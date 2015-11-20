8d51776ae611e3f32d955024e3c9ee8f4fb83499
Joshua Galvin <joshuagalvin@gmail.com>
2015-11-19 17:20:58 -0800

    More updates from this morning.

--- multiaudio.js
+++ multiaudio.js
@@ -1,9 +1,35 @@
 var MultiAudio = (function() {
     "use strict";
     
-    return function(source, opts) {
-        opts = opts || {};
-        opts.size = opts.size || 100; // Buffer Size (100ms)
+    var sync = altspace.utilities.sync.getInstance({
+            appId : "MultiAudio",
+            authorId: "Galvus"
+        }),
+        rate = 24/1000,
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
+        receiveBuffer = audioCtx.createBuffer(1, 
+            audioCtx.sampleRate * rate, 
+            audioCtx.sampleRate),
+        receivePlayer = audioCtx.createBufferSource();
         
+    receivePlayer.buffer = receiveBuffer;
+    receivePlayer.connect(audioCtx.destination);
+    
+    function HostMA(sourceBuffer) {
+        this.source = sourceBuffer;
+        this.interval = setInterval(this.send.bind(this), rate);
+    };
+    
+    HostMA.prototype.stop = function() {
+        clearInterval(this.interval);
+        return this;
+    };
+    
+    HostMA.prototype.send = function() { 
+    };
+    
+    // make it work like MultiCanvas
+    return function(sourceBuffer) {
+        return new HostMA(sourceBuffer);
     };
 } ());
